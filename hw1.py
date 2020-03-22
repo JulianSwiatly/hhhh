a5a7baebe19176b7c0bff5c25da6a35b95a3054b
MrAdios <62249798+MrAdios@users.noreply.github.com>
2020-03-22 19:00:35 +0100

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -1,6 +1,7 @@
 from typing import List
 
 import pandas as pd
+import datetime
 
 CONFIRMED_CASES_URL = f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data" \
                       f"/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv "
@@ -15,24 +16,26 @@ confirmed_cases = pd.read_csv(CONFIRMED_CASES_URL, error_bad_lines=False)
 def poland_cases_by_date(day: int, month: int, year: int = 2020) -> int:
     """
     Returns confirmed infection cases for country 'Poland' given a date.
-
     Ex.
     >>> poland_cases_by_date(7, 3, 2020)
     5
     >>> poland_cases_by_date(11, 3)
     31
-
     :param year: 4 digit integer representation of the year to get the cases for, defaults to 2020
     :param day: Day of month to get the cases for as an integer indexed from 1
     :param month: Month to get the cases for as an integer indexed from 1
     :return: Number of cases on a given date as an integer
     """
-    
-    # Your code goes here (remove pass)
-    pass
+    d=datetime.date(year,month,day)
+    data=d.strftime('%m/%d/%y').lstrip("0").replace("/0","/")
+    pol_cases = confirmed_cases.loc[confirmed_cases["Country/Region"]=="Poland"]
+    result=pol_cases[data].values[0]
+    return result
 
 
 def top5_countries_by_date(day: int, month: int, year: int = 2020) -> List[str]:
+    
+    
     """
     Returns the top 5 infected countries given a date (confirmed cases).
 
@@ -47,9 +50,10 @@ def top5_countries_by_date(day: int, month: int, year: int = 2020) -> List[str]:
     :param year: Month to get the countries for as an integer indexed from 1
     :return: A list of strings with the names of the coutires
     """
-
-    # Your code goes here (remove pass)
-    pass
+    d2=datetime.date(year,month,day)
+    data=d2.strftime('%m/%d/%y').lstrip("0").replace("/0","/")
+    top=confirmed_cases[["Country/Region",d2]].groupby(["Country/Region"]).sum().sort_values(by=d2, ascending=False).head(5)
+    return top.index
 
 # Function name is wrong, read the pydoc
 def no_new_cases_count(day: int, month: int, year: int = 2020) -> int:
@@ -69,5 +73,10 @@ def no_new_cases_count(day: int, month: int, year: int = 2020) -> int:
     :return: Number of countries/regions where the count has not changed in a day
     """
     
-    # Your code goes here (remove pass)
-    pass
+    k=datetime.date(2020,3,7)
+    kata=k.strftime('%m/%d/%y').lstrip("0").replace("/0","/")
+    kd=k-datetime.timedelta(days=1)
+    kata2=kd.strftime('%m/%d/%y').lstrip("0").replace("/0","/")
+    print(kata2)
+    ilosc = len(confirmed_cases.loc[confirmed_cases[kata]-confirmed_cases[kata2]!=0])
+    return (ilosc.index)
